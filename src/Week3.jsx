bd8db8bbe08b5515dbbbcca2a8688b7eef4db5fc
reeny404 <happy2hyeon@kakao.com>
2024-06-13 23:54:44 +0900

    feat: redux 코드 제거

--- src/Week3.jsx
+++ src/Week3.jsx
@@ -1,7 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import store from "./redux/store";
 import "./reset.css";
 import router from "./routes/router";
 
@@ -13,9 +11,7 @@ export default function Week3() {
       className="w-full h-full min-h-[100vh] bg-[#8aa6a3] box-border"
     >
       <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <RouterProvider router={router} />
-        </Provider>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </div>
   );
